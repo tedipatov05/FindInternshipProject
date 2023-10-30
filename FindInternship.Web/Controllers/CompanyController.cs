b8fa2ae317b66e0aba59f4a6be54abb5c9547a3b
tedipatov05 <teodorpatov05@pmgkk.com>
2023-10-30 22:52:09 +0200

    Added Contact View and Demo all companies view

---
+++ FindInternship.Web/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FindInternship.Web.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
--- FindInternship.Web/Controllers/HomeController.cs
+++ FindInternship.Web/Controllers/HomeController.cs
@@ -31,6 +31,11 @@ namespace FindInternship.Web.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
