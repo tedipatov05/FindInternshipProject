ef5a0c1df6ac993f642708f88daa58d5396ee2b9
Teodor <teodorpatov05@pmgkk.com>
2024-01-08 22:51:42 +0200

    changed logic for sending messages

--- FindInternship.Web/wwwroot/js/privateChat.js
+++ FindInternship.Web/wwwroot/js/privateChat.js
@@ -53,6 +53,37 @@ connection.on("SendMessage", function (userId, fromUsername, fromUserImage, mess
     let formattedDate =
         `${dateTime.getDate()}-${(dateTime.getMonth() + 1)}-${dateTime.getFullYear()} ${dateTime.getHours()}:${dateTime.getMinutes()}`;
 
+
+    let chatHolder = document.getElementById('chatHolder');
+
+    let div = document.createElement('div');
+    div.classList.add('d-flex');
+
+    div.innerHTML = `<div style="margin-right: 15px">
+                        <img src="${fromUserImage}" class="rounded-circle shadow avatar-sm mr-3" alt="Profile Picture">
+                     </div>
+                     <div class="left-chat-message fs-13 mb-2">
+                         <p class="mb-3 mr-3 pr-4">${msg}</p>
+                         <div class="message-options">
+                             <div class="message-time">${formattedDate}</div>
+                             <div class="message-arrow"><i class="text-muted la la-angle-down fs-17"></i></div>
+                         </div>
+                     </div>`;
+
+    chatHolder.appendChild(div);
+    //TODO: update scroling messages
+
+});
+
+connection.on("ReceiveMessage", function (fromUserName, fromUserImage, message) {
+  
+
+
+    let msg = message;
+    let dateTime = new Date()
+    let formattedDate =
+        `${dateTime.getDate()}-${(dateTime.getMonth() + 1)}-${dateTime.getFullYear()} ${dateTime.getHours()}:${dateTime.getMinutes()}`;
+
     var chatHolder = document.getElementById('chatHolder');
 
     var msgDiv = document.createElement('div');
@@ -85,34 +116,6 @@ connection.on("SendMessage", function (userId, fromUsername, fromUserImage, mess
 
     chatHolder.appendChild(msgDiv);
 
-    //TODO: update scroling messages
-
-});
-
-connection.on("ReceiveMessage", function (fromUserName, fromUserImage, message) {
-    let msg = message;
-    let dateTime = new Date()
-    let formattedDate =
-        `${dateTime.getDate()}-${(dateTime.getMonth() + 1)}-${dateTime.getFullYear()} ${dateTime.getHours()}:${dateTime.getMinutes()}`;
-
-
-    let chatHolder = document.getElementById('chatHolder');
-
-    let div = document.createElement('div');
-    div.classList.add('d-flex');
-
-    div.innerHTML = `<div style="margin-right: 15px">
-                        <img src="${fromUserImage}" class="rounded-circle shadow avatar-sm mr-3" alt="Profile Picture">
-                     </div>
-                     <div class="left-chat-message fs-13 mb-2">
-                         <p class="mb-3 mr-3 pr-4">${msg}</p>
-                         <div class="message-options">
-                             <div class="message-time">${formattedDate}</div>
-                             <div class="message-arrow"><i class="text-muted la la-angle-down fs-17"></i></div>
-                         </div>
-                     </div>`;
-
-    chatHolder.appendChild(div);
 
 
 })
