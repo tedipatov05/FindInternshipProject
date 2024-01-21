23c332f0a69e199570f9b71cac95704ed9202826
Teodor <108554719+tedipatov05@users.noreply.github.com>
2024-01-21 11:25:22 +0200

    Added view for all abilities

---
+++ FindInternship.Web/Areas/Admin/Controllers/AbilityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FindInternship.Web.Areas.Admin.Controllers
+{
+    public class AbilityController : BaseController
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
