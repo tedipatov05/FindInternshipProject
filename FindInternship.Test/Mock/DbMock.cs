06d8afb83f2ed664bd784beb5baef4fbb9e95394
Teodor <108554719+tedipatov05@users.noreply.github.com>
2024-01-30 12:27:40 +0200

    Added inMemoryDb for testing

---
+++ FindInternship.Test/Mock/DbMock.cs
@@ -0,0 +1,25 @@
+﻿using FindInternship.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindInternship.Test.Mock
+{
+    public class DbMock
+    {
+        public static FindInternshipDbContext Instance
+        {
+            get
+            {
+                var options = new DbContextOptionsBuilder<FindInternshipDbContext>()
+                    .UseInMemoryDatabase("FindInternshipInMemoryDb" + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+                return new FindInternshipDbContext(options, false);
+            }
+        }
+    }
+}
