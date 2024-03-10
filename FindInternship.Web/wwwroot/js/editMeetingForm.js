e06df2ce4ed80099eb8fca6d044d5e632eda3ade
tedipatov05 <108554719+tedipatov05@users.noreply.github.com>
2024-03-10 23:10:53 +0200

    fixed bug with editing meeting

--- FindInternship.Web/wwwroot/js/editMeetingForm.js
+++ FindInternship.Web/wwwroot/js/editMeetingForm.js
@@ -1,14 +1,14 @@
 $(document).ready(function ($) {
 
-    document.getElementById('start-date').addEventListener('focus', function () {
-        this.type = 'datetime-local'
-        document.getElementById('label-start').style = 'transform: translateY(-50%) scale(0.8);background-color: #212121; padding: 0 0.2em; color: #2196f3;'
-    });
+    //document.getElementById('start-date').addEventListener('focus', function () {
+    //    this.type = 'datetime-local'
+    //    document.getElementById('label-start').style = 'transform: translateY(-50%) scale(0.8);background-color: #212121; padding: 0 0.2em; color: #2196f3;'
+    //});
 
-    document.getElementById('end-date').addEventListener('focus', function () {
-        this.type = 'datetime-local'
-        document.getElementById('label-end').style = 'transform: translateY(-50%) scale(0.8);background-color: #212121; padding: 0 0.2em; color: #2196f3;'
-    });
+    //document.getElementById('end-date').addEventListener('focus', function () {
+    //    this.type = 'datetime-local'
+    //    document.getElementById('label-end').style = 'transform: translateY(-50%) scale(0.8);background-color: #212121; padding: 0 0.2em; color: #2196f3;'
+    //});
 
 
     let filesContainer = document.getElementById('files-container');
