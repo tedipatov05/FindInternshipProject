2d2f3efd3895100f204347f39842d60e997211ce
Ivon06 <ivonmircheva2@gmail.com>
2024-04-10 22:53:19 +0300

    Blog started

---
+++ FindInternship.Web/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FindInternship.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        public async Task<IActionResult> BlogHome()
+        {
+            return View();
+        }
+    }
+}
